0486ea2ba59ea29cfcecc57e048b44414af680fd
Enzo Martinez <killerbackofheart@gmail.com>
2022-07-16 20:03:16 +0200

    Premier pas dans le projet

---
+++ Shogi/Shogi/src/constants.py
@@ -0,0 +1,13 @@
+import pygame
+import os
+
+widthS, heightS = 800, 800
+rows, cols = 9,9
+square = widthS//rows
+
+brown = (87,16,16)
+white = (255,255,255)
+
+#Black pieces
+#Exemple :
+#blackKnight = pygame.transform.scale(pygame.image.load(os.path.join(".\images", "bKN.png")), (square, square))
