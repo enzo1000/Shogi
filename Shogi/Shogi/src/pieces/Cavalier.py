817da29f02b793c173b57c3da4ea32e801f4731f
Enzo <enzo111001@live.fr>
2022-07-23 23:32:24 +0200

    Ajout du Cavalier et du lancier (sans respecter l'héritage avec tour)

--- Shogi/Shogi/src/pieces/Cavalier.py
+++ Shogi/Shogi/src/pieces/Cavalier.py
@@ -1,8 +1,28 @@
 from Pieces import *
 
 class Cavalier(Pieces):
-    pass
+   def __init__(self, square, image, cote, type, row, col):
+        super().__init__(square, image, cote, type, row, col)
 
+    def getAvailableMoves(self, row, col, Board):
+        self.clearAvailableMoves()
 
+        if self.cote == Regnant:            #Si Regnant alors joueur est en bas du plateau
+            if row - 2 >= 0 and col + 1 < len(Board):
+                if Board[row-2][col+1] == 0 or Board[row-2][col+1].cote != self.cote:
+                    self.availableMoves.append((row-2, col+1))
 
+            if row - 2 >= 0 and col - 1 >= 0:
+                if Board[row-2][col-1] == 0 or Board[row-2][col-1].cote != self.cote:
+                    self.availableMoves.append((row-2, col-1))
 
+        if self.cote == Opposant:   
+            if row + 2 < len(Board[row]) and col + 1 < len(Board):
+                if Board[row+2][col+1] == 0 or Board[row-2][col+1].cote != self.cote:
+                    self.availableMoves.append((row+2, col+1))
+
+            if row + 2 < len(Board[row]) and col - 1 >= 0:
+                if Board[row+2][col-1] == 0 or Board[row+2][col-1].cote != self.cote:
+                    self.availableMoves.append((row+2, col-1))
+
+        return self.availableMoves
---
+++ Shogi/Shogi/src/pieces/Lancier.py
@@ -0,0 +1,36 @@
+from Pieces import *
+
+class Lancier(Pieces):
+    def __init__(self, square, image, cote, type, row, col):
+        super().__init__(square, image, cote, type, row, col)
+
+    def getAvailableMoves(self, row, col, Board):
+        self.clearAvailableMoves()
+
+        if self.cote == Regnant:            #Si Regnant alors joueur est en bas du plateau
+            for j in range(row-1, -1, -1):  #Vers le haut du plateau
+
+            if Board[j][col] == 0:
+                self.availableMoves.append((j, col))
+
+            elif Board[j][col].cote != self.color:
+                self.availableMoves.append((j, col))
+                break
+            break   #Je crois que ce break ne sert à rien
+            
+
+        if self.cote == Opposant:   
+            for i in range(row+1, len(Board)): #Vers le bas jusqu'au bors du tableau
+
+                if Board[i][col] == 0:
+                    self.availableMoves.append((i, col))
+
+                elif Board[i][col].cote != self.color:
+                    self.availableMoves.append((i, col))
+                    break
+                break
+
+
+
+
+
