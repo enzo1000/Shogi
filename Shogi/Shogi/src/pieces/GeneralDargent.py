8b628c0bae30990950daf1b2aa0aa6d321659561
Enzo <enzo111001@live.fr>
2022-08-13 00:06:41 +0200

    Fin GA et GO

--- Shogi/Shogi/src/pieces/GeneralDargent.py
+++ Shogi/Shogi/src/pieces/GeneralDargent.py
@@ -4,8 +4,33 @@ class GeneralDargent(Pieces):
     def __init__(self, square, image, cote, type, row, col):
         super().__init__(square, image, cote, type, row, col)
 
+    def generalLigneHautBas(self, rowi, Board):
+         for coli in [self.col-1, self.col, self.col+1]:     #On fait la ligne du haut en premier (les 3 cases en haut de la piece)
+             if coli != self.col or rowi != self.row:
+                if Board[rowi][coli] >= 0 and Board[rowi][coli] < len(Board[self.row]):   #Si on ne déborde pas alors 
+                    if Board[rowi][coli] == 0:
+                        self.availableMoves.append((rowi, coli))
+                    elif Board[rowi][coli].cote != self.cote:
+                        self.availableMoves.append((rowi, coli))
+
     def getAvailableMoves(self, row, col, Board):
-        pass
+        self.clearAvailableMoves()
+
+        if self.cote == Joueur.Opposant:
+            self.generalLigneHautBas(self, row - 1, Board)
+            if Board[row + 1][col + 1] == 0:
+                self.availableMoves.append((row + 1, col + 1))
+
+            if Board[row + 1][col - 1] == 0:
+                self.availableMoves.append((row + 1, col - 1))
+
+        else:
+            self.generalLigneHautBas(self, row + 1, Board)
+            if Board[row - 1][col + 1] == 0:
+                self.availableMoves.append((row - 1, col + 1))
+
+            if Board[row - 1][col - 1] == 0:
+                self.availableMoves.append((row - 1, col - 1))
 
 
 
--- Shogi/Shogi/src/pieces/GeneralDor.py
+++ Shogi/Shogi/src/pieces/GeneralDor.py
@@ -4,13 +4,29 @@ class GeneralDor(Pieces):
     def __init__(self, square, image, cote, type, row, col):
         super().__init__(square, image, cote, type, row, col)
 
+    def generalLigneHautBas(self, rowi, Board):
+         for coli in [self.col-1, self.col, self.col+1]:     #On fait la ligne du haut en premier (les 3 cases en haut de la piece)
+             if coli != self.col or rowi != self.row:
+                if Board[rowi][coli] >= 0 and Board[rowi][coli] < len(Board[self.row]):   #Si on ne déborde pas alors 
+                    if Board[rowi][coli] == 0:
+                        self.availableMoves.append((rowi, coli))
+                    elif Board[rowi][coli].cote != self.cote:
+                        self.availableMoves.append((rowi, coli))
+
     def getAvailableMoves(self, row, col, Board):
-        for coli in [self.col-1, self.col, self.col+1]:     #On fait la ligne du haut en premier (les 3 cases en haut de la piece)
-            if Board[row][coli] >= 0 and Board[row][coli] < len(Board[self.row]):   #Si on ne déborde pas alors 
-                if Board[row][coli] == 0:
-                    self.availableMoves.append((row, coli))
-                elif Board[row][coli].cote != self.cote:
-                    self.availableMoves.append((row, coli))
+        self.clearAvailableMoves()
+
+        if self.cote == Joueur.Opposant:
+            self.generalLigneHautBas(self, row - 1, Board)
+            self.generalLigneHautBas(self, row, Board)
+            if Board[row + 1][col] == 0:
+                self.availableMoves.append((row + 1, col))
+
+        else:
+            self.generalLigneHautBas(self, row + 1, Board)
+            self.generalLigneHautBas(self, row, Board)
+            if Board[row - 1][col] == 0:
+                self.availableMoves.append((row - 1, col))
 
 
 
